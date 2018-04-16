b4f5843225e6f054c1a53af968dc594c9d7a55c4
okputadora <okputadora@gmail.com>
2018-04-16 11:03:17 -0400

    twitter functionality

--- liri.js
+++ liri.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const keys = require('./keys')
-const spotify = new Spotify(keys.spotify);
+const Twitter = require('twitter')
+// const Spotify = require('node-spotify-api')
+// const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
 
 const command = process.argv[2]
-const param = process.argv.slice(3).join(" ")
+const param = process.argv.slice(3).join(' ')
 
 // Decipher command
 switch (command){
@@ -17,12 +19,21 @@ switch (command){
   case 'movie-this':
     getMovieInfo(param)
     break;
-  case 'do-what-it-says'
+  case 'do-what-it-says':
     randomCommand()
     break;
-  default
-    console.log("Your command is not recognized")
+  default:
+    console.log('Your command is not recognized')
 }
 
 
 // Get tweets
+function getTweets(){
+  client.get('search/tweets', {q: 'okputadora'}, function(error, tweets, response) {
+   console.log('most recent tweets from ', tweets.statuses[0].user.screen_name)
+   tweets.statuses.forEach(tweet => {
+     console.log('text: ', tweet.text)
+     console.log('created: ',tweet.created_at)
+   })
+});
+}
