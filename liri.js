98638794e03a371ff8ff1fa7135c30cdd0b0b495
okputadora <okputadora@gmail.com>
2018-04-16 11:21:42 -0400

    spotify functionality

--- liri.js
+++ liri.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const keys = require('./keys')
 const Twitter = require('twitter')
-// const Spotify = require('node-spotify-api')
-// const spotify = new Spotify(keys.spotify);
+const Spotify = require('node-spotify-api')
+const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
 
 const command = process.argv[2]
@@ -30,10 +30,28 @@ switch (command){
 // Get tweets
 function getTweets(){
   client.get('search/tweets', {q: 'okputadora'}, function(error, tweets, response) {
-   console.log('most recent tweets from ', tweets.statuses[0].user.screen_name)
-   tweets.statuses.forEach(tweet => {
+    console.log('most recent tweets from ', tweets.statuses[0].user.screen_name)
+    tweets.statuses.forEach(tweet => {
      console.log('text: ', tweet.text)
      console.log('created: ',tweet.created_at)
-   })
-});
+    })
+  });
+}
+
+// Get song info
+function getSongInfo(song){
+  spotify
+  .search({ type: 'track', query: song, limit: 1})
+  .then(function(response) {
+    console.log('Song: ', response.tracks.items[0].name);
+    console.log('Album: ', response.tracks.items[0].album.name);
+    console.log('Artist: ', response.tracks.items[0].artists[0].name);
+    console.log('Link: ', response.tracks.items[0].external_urls.spotify);
+    // response.tracks.items.forEach(song => {
+    //   console.log(song)
+    // })
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 }
